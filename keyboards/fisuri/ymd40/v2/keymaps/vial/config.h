ff384c13b5d15fb315a3a2bd961e38b198e1beb8
Yaroslav Malykh <fisuri@murena.io>
2025-01-17 20:26:29 +0400

    ymd40: add

---
+++ keyboards/fisuri/ymd40/v2/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x5B, 0x59, 0x93, 0xC4, 0x41, 0xEA, 0x65, 0x0F}
+
+#define VIAL_COMBO_ENTRIES 8
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_KEY_OVERRIDE_ENTRIES 4
