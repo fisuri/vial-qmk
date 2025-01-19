adb9e3928fd92417a80d41738e6338d8a1d1637c
Yaroslav Malykh <fisuri@murena.io>
2025-01-19 15:53:32 +0400

    flake: update 19.01.2025

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
 {
-  description = "test";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
@@ -13,17 +12,9 @@
     pkgs = nixpkgs.legacyPackages;
   in {
     packages = forAllSystems (system: {
-      buildMaster = pkgs.${system}.callPackage ./default.nix {buildType = "master";};
-      buildSlave = pkgs.${system}.callPackage ./default.nix {buildType = "slave";};
-      buildAll = pkgs.${system}.callPackage ./default.nix {buildType = "all";};
+      buildLotus58 = pkgs.${system}.callPackage ./default.nix {buildKeyboard = "lotus58";};
+      buildYmd40v2 = pkgs.${system}.callPackage ./default.nix {buildKeyboard = "ymd40";};
     });
-
-    # packages = forAllSystems.buildSlave (system: {
-    #   default = pkgs.${system}.callPackage ./default.nix {};
-    # });
-    # packages = forAllSystems.buildAll (system: {
-    #   default = pkgs.${system}.callPackage ./default.nix {};
-    # });
 
     devShells = forAllSystems (system: {
       default = pkgs.${system}.callPackage ./shell.nix {};
