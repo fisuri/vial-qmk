b1e4573c529341fd9eed4215b2a860dfc5e1f1ef
Yaroslav Malykh <fisuri@murena.io>
2025-02-04 02:07:28 +0400

    add klor keyboard

--- keyboards/fisuri/klor/config.h
+++ keyboards/fisuri/klor/config.h
@@ -2,16 +2,16 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
-#ifdef POINTING_DEVICE_ENABLE
-#    define POINTING_DEVICE_SCLK_PIN D0
-#    define POINTING_DEVICE_SDIO_PIN D1
-#endif
+// #ifdef POINTING_DEVICE_ENABLE
+// #    define POINTING_DEVICE_SCLK_PIN D0
+// #    define POINTING_DEVICE_SDIO_PIN D1
+// #endif
 
-#ifdef AUDIO_ENABLE
-#    define AUDIO_PIN B5
-#endif
+// #ifdef AUDIO_ENABLE
+// #    define AUDIO_PIN B5
+// #endif
 
-#ifdef OLED_ENABLE
-#    define OLED_DISPLAY_128X64
-#    define OLED_FONT_H "keyboards/geigeigeist/klor/glcdfont.c"
-#endif
+// #ifdef OLED_ENABLE
+// #    define OLED_DISPLAY_128X64
+// #    define OLED_FONT_H "keyboards/geigeigeist/klor/glcdfont.c"
+// #endif
---
+++ keyboards/fisuri/klor/keymaps/vial/config.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define MASTER_LEFT
+
+#define VIAL_KEYBOARD_UID {0xBA, 0x38, 0x22, 0x3F, 0x8C, 0x42, 0x0F, 0x44}
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+// VIAL specific reduce memory
+#define VIAL_COMBO_ENTRIES 8
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_KEY_OVERRIDE_ENTRIES 4
