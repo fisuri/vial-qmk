8e74837785c151ce860c1a1fb5a92cb2000b182d
Yaroslav Malykh <fisuri@murena.io>
2025-02-28 16:04:32 +0400

    sofle: update keymaps

--- keyboards/fisuri/sofle/keymaps/vial/keymap.c
+++ keyboards/fisuri/sofle/keymaps/vial/keymap.c
@@ -29,13 +29,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                          KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,
    KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                          KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
    KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,    KC_MUTE, KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS,
-                       KC_LCTL,  KC_LALT,  KC_LGUI,  MO(1),    KC_SPC,     KC_ENT,  MO(2),    KC_BSPC,  KC_DEL,   KC_LCAP
+                       KC_LCTL,  KC_LALT,  KC_LGUI,  MO(1),    KC_SPC,     KC_ENT,  MO(2),    KC_BSPC,  KC_DEL,   KC_CAPS
 ),
 [1] = LAYOUT(
 //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷
    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MINS,  XXXXXXX,
-   XXXXXXX,  KC_PGUP,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       KC_EQL,   KC_7,     KC_8,     KC_9,     KC_PLUS,  XXXXXXX,
-   _______,  KC_PGDN,  KC_HOME,  KC_END,   KC_INS,   KC_PSCR,                       KC_0,     KC_4,     KC_5,     KC_6,     KC_SLSH,  XXXXXXX,
+   XXXXXXX,  KC_PGUP,  KC_END,   KC_UP,    KC_HOME,  XXXXXXX,                       KC_EQL,   KC_7,     KC_8,     KC_9,     KC_PLUS,  XXXXXXX,
+   _______,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PSCR,                       KC_0,     KC_4,     KC_5,     KC_6,     KC_SLSH,  XXXXXXX,
    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,   _______,  KC_DOT,   KC_1,     KC_2,     KC_3,     KC_ASTR,  XXXXXXX,
                        _______,  _______,  _______,  _______,  _______,   _______,  MO(3),    _______,  _______,  _______
 ),
