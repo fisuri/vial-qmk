4eb8e730cda8e9835c8ed6b8c1e058e0fb53327a
Yaroslav Malykh <fisuri@murena.io>
2025-03-13 18:22:30 +0400

    klor: update configurations

--- keyboards/fisuri/klor/keymaps/vial/config.h
+++ keyboards/fisuri/klor/keymaps/vial/config.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 
 #define VIAL_KEYBOARD_UID {0xBA, 0x38, 0x22, 0x3F, 0x8C, 0x42, 0x0F, 0x44}
 
@@ -10,6 +10,6 @@
 #undef LOCKING_RESYNC_ENABLE
 
 // VIAL specific reduce memory
-#define VIAL_COMBO_ENTRIES 8
-#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 6
+#define VIAL_TAP_DANCE_ENTRIES 6
 #define VIAL_KEY_OVERRIDE_ENTRIES 4
--- keyboards/fisuri/klor/keymaps/vial/keymap.c
+++ keyboards/fisuri/klor/keymaps/vial/keymap.c
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷
               KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                          KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  
     KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                          KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MPLY,   KC_MPLY,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-                                  KC_LGUI,  MO(1),    KC_SPC,   KC_ESC,    KC_LALT,  KC_ENT,   MO(2),    KC_BSPC
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,   KC_MPLY,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS,
+                                  KC_LGUI,  MO(1),    KC_SPC,   KC_LCTL,   KC_LALT,  KC_ENT,   MO(2),    KC_BSPC
  ),
 
 /*
@@ -50,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [1] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷
-              KC_PGUP,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       KC_EQL,   KC_P7,    KC_P8,    KC_P9,    KC_PPLS,  
-    _______,  KC_PGDN,  KC_HOME,  KC_END,   KC_INS,   KC_PSCR,                       KC_P0,    KC_P4,    KC_P5,    KC_P6,    KC_PAST,  KC_NUM,
-    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,   _______,  KC_PDOT,  KC_P1,    KC_P2,    KC_P3,    KC_PMNS,  XXXXXXX,
-                                  _______,  _______,  _______,  _______,   _______,  _______,  MO(3),    _______
+              KC_PGUP,  KC_END,   KC_UP,    KC_HOME,  XXXXXXX,                       KC_EQL,   KC_P7,    KC_P8,    KC_P9,    KC_PPLS,  
+    KC_ESC,   KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PSCR,                       KC_P0,    KC_P4,    KC_P5,    KC_P6,    KC_PAST,  KC_NUM,
+    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_INS,   _______,   _______,  KC_PDOT,  KC_P1,    KC_P2,    KC_P3,    KC_PMNS,  XXXXXXX,
+                                  _______,  _______,  _______,  QK_BOOT,   QK_BOOT,  _______,  MO(3),    _______
  ),
 
 /*
@@ -99,10 +99,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [3] = LAYOUT(
   //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷
-              QK_BOOT,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  QK_BOOT,  
+              XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  
     XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                                   _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______
+    XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,   _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+                                   _______,  _______,  _______,  QK_BOOT,   QK_BOOT,  _______,  _______,  _______
  )
 
  /*
@@ -126,18 +126,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               _______,  _______,  _______,  _______,  _______,                       _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,                       _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,
-              _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______
+                                  _______,  _______,  _______,  _______,   _______, _______,  _______,  _______
  )*/
 };
 
 #ifdef ENCODER_MAP_ENABLE
 
   const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-      //                  Encoder 1                                     Encoder 2
+      //    Encoder 1                         Encoder 2
   };
 
 #endif
