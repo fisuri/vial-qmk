36dc3118d75fe971fe8632874743cab785ff03fb
Yaroslav Malykh <fisuri@murena.io>
2025-04-16 21:54:53 +0400

    sofle: update keymap

--- keyboards/fisuri/sofle/keymaps/vial/keymap.c
+++ keyboards/fisuri/sofle/keymaps/vial/keymap.c
@@ -28,21 +28,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                          KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_GRV,
    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                          KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,
    KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                          KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-   KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,    KC_MUTE, KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS,
-                       KC_LCTL,  KC_LALT,  KC_LGUI,  MO(1),    KC_SPC,     KC_ENT,  MO(2),    KC_BSPC,  KC_DEL,   KC_CAPS
+   KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,    KC_MUTE, KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RBRC,
+                       KC_CAPS,  KC_LALT,  KC_LGUI,  MO(1),    KC_SPC,     KC_ENT,  MO(2),    KC_BSPC,  KC_DEL,   KC_BSLS 
 ),
 [1] = LAYOUT(
 //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷
-   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MINS,  XXXXXXX,
-   XXXXXXX,  KC_PGUP,  KC_END,   KC_UP,    KC_HOME,  XXXXXXX,                       KC_EQL,   KC_7,     KC_8,     KC_9,     KC_PLUS,  XXXXXXX,
-   _______,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PSCR,                       KC_0,     KC_4,     KC_5,     KC_6,     KC_SLSH,  XXXXXXX,
-   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,   _______,  KC_DOT,   KC_1,     KC_2,     KC_3,     KC_ASTR,  XXXXXXX,
+   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                         KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+   XXXXXXX,  KC_PGUP,  KC_END,   KC_UP,    KC_HOME,  XXXXXXX,                       KC_EQL,   KC_7,     KC_8,     KC_9,     KC_SLSH,  KC_ASTR,
+   _______,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PSCR,                       KC_0,     KC_4,     KC_5,     KC_6,     KC_MINS,  KC_PLUS,
+   _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,   _______,  KC_DOT,   KC_1,     KC_2,     KC_3,     XXXXXXX,  XXXXXXX,
                        _______,  _______,  _______,  _______,  _______,   _______,  MO(3),    _______,  _______,  _______
 ),
 [2] = LAYOUT(
 //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷
    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                         KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
-   KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                       KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_BSPC,
+   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,    _______, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
                        _______,  _______,  _______,  MO(3),    _______,    _______, _______,  _______,  _______,  _______
